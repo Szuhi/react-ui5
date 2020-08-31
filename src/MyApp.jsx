d2f5632ee43117753ea832521d335fae377406e1
Szuhanszky <szuhanszky.tamas@gmail.com>
2020-08-31 15:28:22 +0200

    Clickable header for the Card

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Card, Text } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
 
 export function MyApp() {
+    const handleHeaderClick = () => {
+        alert ("Header clicked");
+    }
+
     return (
         <div>
-            <Card heading="Card">
-                <Text>This is the content area of the Card</Text>
+            <Card 
+                heading="Card" 
+                style={{ width: "300px" }} 
+                headerInteractive
+                onHeaderClick={handleHeaderClick}>
+                <Text style={spacing.sapUiContentPadding}>
+                    This is the content area of the Card
+                </Text>
             </Card>
         </div>
     )
