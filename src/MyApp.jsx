377aa60e3fc9ea6c1468bae913ca1a80eae00afd
Szuhanszky <szuhanszky.tamas@gmail.com>
2020-08-31 15:23:42 +0200

    Adding content to the card

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Card, Text } from "@ui5/webcomponents-react";
 
 export function MyApp() {
-    return <div>My root component</div>
+    return (
+        <div>
+            <Card heading="Card">
+                <Text>This is the content area of the Card</Text>
+            </Card>
+        </div>
+    )
 }
