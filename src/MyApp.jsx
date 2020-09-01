39f97cedadac364feca72c994c1bea85f5528498
Szuhanszky <szuhanszky.tamas@gmail.com>
2020-09-01 10:26:33 +0200

    Custom list on new card

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import {
-  Avatar,
-  Card,
-  Text,
-  ShellBar,
-  ShellBarItem,
-  List,
-  StandardListItem,
-  ValueState,
-  ProgressIndicator,
-  Title,
-  TitleLevel,
-  FlexBox,
-  FlexBoxJustifyContent,
-  FlexBoxWrap,
-  FlexBoxDirection,
-  AnalyticalTable,
-  Icon
+    Avatar,
+    Card,
+    Text,
+    ShellBar,
+    ShellBarItem,
+    List,
+    StandardListItem,
+    ValueState,
+    ProgressIndicator,
+    Title,
+    TitleLevel,
+    FlexBox,
+    FlexBoxJustifyContent,
+    FlexBoxWrap,
+    FlexBoxDirection,
+    AnalyticalTable,
+    Icon
 } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
 import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
@@ -27,102 +27,100 @@ import "@ui5/webcomponents-icons/dist/icons/list.js";
 import "@ui5/webcomponents-icons/dist/icons/table-view.js";
 
 export function MyApp() {
-  const [toggleCharts, setToggleCharts] = useState("lineChart");
-  const [loading, setLoading] = useState(false);
-  const contentTitle =
-    toggleCharts === "lineChart" ? "Line Chart" : "Bar Chart";
-  const switchToChart =
-    toggleCharts === "lineChart" ? "Bar Chart" : "Line Chart";
+    const [toggleCharts, setToggleCharts] = useState("lineChart");
+    const [loading, setLoading] = useState(false);
+    const contentTitle =
+        toggleCharts === "lineChart" ? "Line Chart" : "Bar Chart";
+    const switchToChart =
+        toggleCharts === "lineChart" ? "Bar Chart" : "Line Chart";
 
-  const handleHeaderClick = () => {
-    if (toggleCharts === "lineChart") {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        setToggleCharts("barChart");
-      }, 2000);
-    } else {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        setToggleCharts("lineChart");
-      }, 2000);
-    }
-  };
+    const handleHeaderClick = () => {
+        if (toggleCharts === "lineChart") {
+            setLoading(true);
+            setTimeout(() => {
+                setLoading(false);
+                setToggleCharts("barChart");
+            }, 2000);
+        } else {
+            setLoading(true);
+            setTimeout(() => {
+                setLoading(false);
+                setToggleCharts("lineChart");
+            }, 2000);
+        }
+    };
 
-  const dataset = [
+    const dataset = [
     {
-      month: "January",
-      data: 65
+        month: "January",
+        data: 65
     },
     {
-      month: "February",
-      data: 59
+        month: "February",
+        data: 59
     },
     {
-      month: "March",
-      data: 80
+        month: "March",
+        data: 80
     },
     {
-      month: "April",
-      data: 81
+        month: "April",
+        data: 81
     },
     {
-      month: "May",
-      data: 56
+        month: "May",
+        data: 56
     },
     {
-      month: "June",
-      data: 55
+        month: "June",
+        data: 55
     },
     {
-      month: "July",
-      data: 40
-    }
-  ];
+        month: "July",
+        data: 40
+    }];
 
-  return (
-    <div>
-      <ShellBar
-        logo={<img src="logo192.png" />}
-        profile={<Avatar image="" />}
-        primaryTitle="My App"
-      >
-        <ShellBarItem icon="add" text="Add" />
-      </ShellBar>
-        <Card
-          avatar={
-            <Icon
-              name={
-                toggleCharts === "lineChart"
-                  ? "line-chart"
-                  : "horizontal-bar-chart"
-              }
-            />
-          }
-          heading="Stock Price"
-          style={{ width: "300px" }}
-          headerInteractive
-          onHeaderClick={handleHeaderClick}
-          subheading={`Click here to switch to ${switchToChart}`}
-        >
-          <Text style={spacing.sapUiContentPadding}>{contentTitle}</Text>
-          {toggleCharts === "lineChart" ? (
-            <LineChart
-              dimensions={[{ accessor: "month" }]}
-              measures={[{ accessor: "data", label: "Stock Price" }]}
-              dataset={dataset}
-              loading={loading}
-            />
-          ) : (
-            <BarChart
-              dimensions={[{ accessor: "month" }]}
-              measures={[{ accessor: "data", label: "Stock Price" }]}
-              dataset={dataset}
-              loading={loading}
-            />
-          )}
-        </Card>
-    </div>
+    return (
+        <div>
+            <ShellBar
+                logo={<img src="logo192.png" />}
+                profile={<Avatar image="" />}
+                primaryTitle="My App"
+            >
+                <ShellBarItem icon="add" text="Add" />
+            </ShellBar>
+            <Card 
+                avatar={<Icon name={toggleCharts === "lineChart" ? "line-chart" : "horizontal-bar-chart"}/>}
+                heading="Stock Price"
+                style={{ width: "300px" }}
+                headerInteractive
+                onHeaderClick={handleHeaderClick}
+                subheading={`Click here to switch to ${switchToChart}`}
+            >
+                <Text style={spacing.sapUiContentPadding}>{contentTitle}</Text>
+                {toggleCharts === "lineChart" ? (
+                    <LineChart
+                        dimensions={[{ accessor: "month" }]}
+                        measures={[{ accessor: "data", label: "Stock Price" }]}
+                        dataset={dataset}
+                        loading={loading}
+                    />
+                ) : (
+                    <BarChart
+                        dimensions={[{ accessor: "month" }]}
+                        measures={[{ accessor: "data", label: "Stock Price" }]}
+                        dataset={dataset}
+                        loading={loading}
+                    />
+                )}
+            </Card>
+            <Card
+                heading="Progress"
+                subheading="List"
+                style={{ width: "300px" }}
+                avatar={<Icon name="list" />}
+            >
+            </Card>
+        </div>
   );
 }
