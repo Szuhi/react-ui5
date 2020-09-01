781266b55fb29cef61980e2f3a34523a026341ae
Szuhanszky <szuhanszky.tamas@gmail.com>
2020-09-01 14:42:27 +0200

    Descriptive message on detail screen

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Title } from "@ui5/webcomponents-react/lib/Title";
 
 export function Detail() {
-    return <Title>Hello World!</Title>
+    return <Title>Navigate back to the main page by pressing the log at the top left.</Title>
 }
